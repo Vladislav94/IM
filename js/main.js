9009efa1e0cdf1a288996e78a98a3e5d31eabb74
Vladislav <vladislavturok@MacBook-Air-Vladislav.local>
2015-09-01 19:52:27 +0300

    comparison done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 $( document ).ready(function() {
 
+    // range-slider 
+
+    try {
+        $('.slider-wrap').rangePlugin();
+
+        $(window).resize(function(){
+          $('.slider-wrap').rangePlugin();
+        });
+    } catch (err) {
+
+    };
+
+
+
     $('.minus').click(function () {
         var $input = $(this).parent().find('input');
         var count = parseInt($input.val()) - 1;
         count = count < 1 ? 1 : count;
         $input.val(count);
-        $input.change();
         return false;
     });
     $('.plus').click(function () {
         var $input = $(this).parent().find('input');
         $input.val(parseInt($input.val()) + 1);
-        $input.change();
         return false;
     });
 
